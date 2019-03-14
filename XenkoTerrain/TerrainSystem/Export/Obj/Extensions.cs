af471f52946487e6e15aae94fa2478c7508dcc01
Tom Groner <hansolo128@hotmail.com>
2019-03-13 20:39:57 -0400

    adding missing files

---
+++ XenkoTerrain/TerrainSystem/Export/Obj/Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Xenko.Core.Mathematics;
+
+namespace XenkoTerrain.TerrainSystem.Export.Obj
+{
+  public static class Extensions
+  {
+    public static string PrintObj(this Vector2 self, ObjLineType lineType)
+    {
+      return $"{self.X:0.00000} {self.Y:0.00000}";
+    }
+
+    public static string PrintObj(this Vector3 self, ObjLineType lineType)
+    {
+      return $"{self.X:0.00000} {self.Y:0.00000} {self.Z:0.00000}";
+    }
+  }
+}
---
+++ XenkoTerrain/TerrainSystem/Export/Obj/ObjLineType.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+
+namespace XenkoTerrain.TerrainSystem.Export.Obj
+{
+  public enum ObjLineType
+  {
+    None,
+    Position,
+    Normal,
+    TextureCoordinate,
+    Face,
+    MaterialNew,
+    MaterialUse,
+    MaterialAmbient,
+    MaterialAmbientMap,
+    MaterialDiffuse,
+    MaterialDiffuseMap,
+    MaterialSpecular,
+    MaterialSpecularMap,
+    Group,
+    Object,
+    SmoothShading
+  }
+
+  public static class ObjLineTypeExtensions
+  {
+    private static Dictionary<ObjLineType, string> syntaxReference;
+
+    static ObjLineTypeExtensions()
+    {
+      syntaxReference = new Dictionary<ObjLineType, string>()
+      {
+        [ObjLineType.None] = "",
+        [ObjLineType.Position] = "v ",
+        [ObjLineType.Normal] = "vn ",
+        [ObjLineType.TextureCoordinate] = "vt ",
+        [ObjLineType.Face] = "f ",
+        [ObjLineType.MaterialNew] = "newmtl ",
+        [ObjLineType.MaterialUse] = "usemtl ",
+        [ObjLineType.MaterialAmbient] = "Ka ",
+        [ObjLineType.MaterialAmbientMap] = "map_Ka ",
+        [ObjLineType.MaterialDiffuse] = "Kd ",
+        [ObjLineType.MaterialDiffuseMap] = "map_Kd ",
+        [ObjLineType.MaterialSpecular] = "Ks ",
+        [ObjLineType.MaterialSpecularMap] = "map_Ks ",
+        [ObjLineType.Group] = "g ",
+        [ObjLineType.Object] = "o "
+      };
+    }
+
+    public static string Syntax(this ObjLineType self)
+    {
+      return syntaxReference[self];
+    }
+  }
+}
